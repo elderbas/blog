a994d8bce69160b89bc4b37497b4290a19ecbee1
bschermerhorn <bschermerhorn@bigsquid.com>
2016-07-27 15:23:58 -0500

    all the stuff to generate the static

---
+++ harp_compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+projDir="/Users/bschermerhorn/SideProjects/elderbas.github.io_blog/ghost-theme"
+outputDir="/Users/bschermerhorn/SideProjects/elderbas.github.io_blog/dist"
+harp compile $projDir $outputDir
