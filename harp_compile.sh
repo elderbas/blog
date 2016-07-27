f0e536edd9e24c1b3d99c4f3d2a234c2dc849026
bschermerhorn <bschermerhorn@bigsquid.com>
2016-07-27 15:32:30 -0500

    ghpages

--- harp_compile.sh
+++ harp_compile.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-projDir="/Users/bschermerhorn/SideProjects/elderbas.github.io_blog/ghost-theme"
-outputDir="/Users/bschermerhorn/SideProjects/elderbas.github.io_blog/dist"
+projDir="/Users/bschermerhorn/SideProjects/elderbas.github.io_blog/kitchenSink"
+outputDir="/Users/bschermerhorn/SideProjects/elderbas.github.io_blog/gh-pages"
 harp compile $projDir $outputDir
+
+cd outputDir
+git add .
+git commit -m "$date"
+git push origin kitchenSink
+
+cd outputDir
+git add .
+git commit -m "$date ghpages"
+git push origin gh-pages
+echo 'published'
---
+++ publish.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+projDir="/Users/bschermerhorn/SideProjects/elderbas.github.io_blog/kitchenSink"
+outputDir="/Users/bschermerhorn/SideProjects/elderbas.github.io_blog/gh-pages"
